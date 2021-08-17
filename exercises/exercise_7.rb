637fb7fda98a6811625900f29cc98d5953d18b2c
Kathy <kathy.tam@mail.mcgill.ca>
2021-08-17 12:45:20 +0000

    Vaildation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Provide store name?"
+input = gets.chomp
+Store.create!(name: input)
+# @store1.employees.create!(first_name: "Elon", last_name: "Musk", hourly_rate: 30)
